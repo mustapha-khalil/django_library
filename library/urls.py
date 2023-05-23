f61f311f2261ba0f719ce15d1c51a182c4ae2b23
mustafa khalil <mustafakh1997@gmail.com>
2023-05-23 21:12:17 +0300

    Added paths and corresponding view to each one of them

--- library/urls.py
+++ library/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
-
 from . import views
 
-urlpatterns = [path("", views.index, name="index")]
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("add/", views.addBook, name="add_book"),
+    path("<int:book_id>/", views.editBook),
+    path("<int:book_id>/delete", views.deleteBook),
+]
--- library/views.py
+++ library/views.py
@@ -1,8 +1,50 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpResponseServerError, JsonResponse
+from .models import Book
+from .forms import BookForm
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("Hello, world. You'are at the polls index.")
+    try:
+        books = Book.objects.all()
+        # Convert books queryset to a list of dictionaries
+        books_data = [
+            {
+                "id": book.id,
+                "title": book.title,
+                "author": book.author,
+                "publication_year": book.publication_year,
+                "category": book.category,
+            }
+            for book in books
+        ]
+        return JsonResponse({"books": books_data})
+
+    except Exception as e:
+        error_message = "Something went wrong: " + str(e)
+        return HttpResponseServerError(error_message)
+
+
+def addBook(request):
+    print("adding Book")
+    if request.method == "POST":
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("index")
+
+    else:
+        form = BookForm()
+
+    context = {"form": form}
+    return render(request, "add_book.html", context)
+
+
+def editBook(request, book_id):
+    return HttpResponse("Your are editing the book %s." % book_id)
+
+
+def deleteBook(request, book_id):
+    return HttpResponse("You are deleting book %d" % book_id)
