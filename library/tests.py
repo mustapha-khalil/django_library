067591a0c23cf4ab1aca90b8ed055f4c0e2d230e
mustafa khalil <mustafakh1997@gmail.com>
2023-05-27 21:17:17 +0300

    created test units for the other views

--- library/tests.py
+++ library/tests.py
@@ -25,10 +25,9 @@ class IndexViewTestCase(TestCase):
             publication_year=datetime.date(2020, 3, 23),
         )
 
-        self.url = reverse("index")
-
     def test_index_view_with_no_search_query(self):
-        response = self.client.get(self.url)
+        url = reverse("index")
+        response = self.client.get(url)
 
         # Assert the response
         self.assertEqual(response.status_code, 200)
@@ -39,7 +38,8 @@ class IndexViewTestCase(TestCase):
         self.assertListEqual(book_titles, expected_titles)
 
     def test_index_view_with_search_query(self):
-        response = self.client.get(self.url, {"search_query": "Author 1"})
+        url = reverse("index")
+        response = self.client.get(url, {"search_query": "Author 1"})
 
         # Assert the response
         self.assertEqual(response.status_code, 200)
@@ -50,8 +50,109 @@ class IndexViewTestCase(TestCase):
         self.assertListEqual(book_titles, expected_titles)
 
     def test_index_view_error_handling(self):
-        response = self.client.get(self.url)
+        url = reverse("index")
+        response = self.client.get(url)
 
         # Assert the response
         self.assertEqual(response.status_code, 500)
         self.assertIn(b"Something went wrong:", response.content)
+
+
+class AddViewTestCase(TestCase):
+    def test_add_book_view(self):
+        url = reverse("add_book")
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "add_book.html")
+
+        # Test POST request with valid form data
+        form_data = {
+            "title": "New Book",
+            "author": "Author",
+            "category": "Fiction",
+            "publication_year": "2023-01-01",
+        }
+        response = self.client.post(url, data=form_data)
+
+        self.assertEqual(response.status_code, 302)  # Redirect to index
+        self.assertRedirects(response, reverse("index"))
+
+        # Test POST request with invalid form data
+        invalid_form_data = {
+            "title": "",
+            "author": "Author",
+            "category": "Fiction",
+            "publication_year": "2000-01-01",
+        }
+        response = self.client.post(url, data=invalid_form_data)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "add_book.html")
+        self.assertFormError(response, "form", "title", "This field is required.")
+
+
+class DeleteViewTestCase(TestCase):
+    def test_delete_book_view(self):
+        book = Book.objects.create(
+            title="Lord of the rings",
+            author="Author 1",
+            category="Fantasy",
+            publication_year=datetime.date(2001, 3, 23),
+        )
+        url = reverse("delete_book", args=[book.id])
+
+        response = self.client.post(url)
+
+        self.assertEqual(response.status_code, 302)  # Redirect to index
+        self.assertRedirects(response, reverse("index"))
+
+        # Assert that the book is deleted
+        self.assertFalse(Book.objects.filter(id=book.id).exists())
+
+        # Test deleting a non-existent book
+        non_existent_url = reverse("delete_book", args=[999])
+        response = self.client.post(non_existent_url)
+
+        self.assertEqual(response.status_code, 500)
+        self.assertIn(b"404: Book not found", response.content)
+
+
+class EditViewTestCase(TestCase):
+    def test_edit_book_view(self):
+        book = Book.objects.create(
+            title="Book 1",
+            author="Author 1",
+            category="Fiction",
+            publication_year=datetime.date(2020, 3, 23),
+        )
+        url = reverse("edit_book", args=[book.id])
+
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "edit_book.html")
+        self.assertContains(response, 'value="Book 1"')
+
+        # Test POST request with valid form data
+        form_data = {
+            "title": "Updated Book",
+            "author": "Author",
+            "category": "Fiction",
+            "publication_year": "2022-01-01",
+        }
+        response = self.client.post(url, data=form_data)
+
+        self.assertEqual(response.status_code, 302)  # Redirect to index
+        self.assertRedirects(response, reverse("index"))
+
+        # Assert that the book is updated
+        updated_book = Book.objects.get(id=book.id)
+        self.assertEqual(updated_book.title, "Updated Book")
+
+        # Test editing a non-existent book
+        non_existent_url = reverse("edit_book", args=[999])
+        response = self.client.post(non_existent_url)
+
+        self.assertEqual(response.status_code, 500)
+        self.assertIn(b"404: Book not found", response.content)
